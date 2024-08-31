894300e29d4e97d3a8a7b33ec245c8e7b5af866d
Fayaz409 <ftunio06@gmail.com>
2024-09-01 01:22:12 +0500

    Todo Application With React(Front-end) and Django(Backend)

---
+++ backend/backend/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path,include
+from api.views import *
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView
+)
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',include('api.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='get_token'),
+    path('api/user/register',CreateUserView.as_view(),name='register'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='refresh_token'),
+    path('api-auth/',include('rest_framework.urls')),
+]
